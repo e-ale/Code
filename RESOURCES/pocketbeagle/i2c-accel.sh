476197371b2332f47cbddbf80969f2057e0f6029
Demo User <jdk@ti.com>
2018-03-05 07:31:57 +0000

    pocketbeagle: wip

---
+++ RESOURCES/pocketbeagle/i2c-accel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$EUID" != "0" ];
+  then echo "Please run as root"
+  exit
+fi
+[ -e /sys/kernel/config/device-tree/overlays/accel ] && rmdir /sys/kernel/config/device-tree/overlays/accel
+mkdir -p /sys/kernel/config/device-tree/overlays/accel
+dtc -W no-unit_address_vs_reg -@ -o /sys/kernel/config/device-tree/overlays/accel/dtbo i2c-accel.dts
+sleep 2
+cd /sys/bus/iio/devices/iio\:device1
+watch -n0 cat in_accel_x_raw in_accel_y_raw in_accel_z_raw 
--- RESOURCES/pocketbeagle/spidev.sh
+++ RESOURCES/pocketbeagle/spidev.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 if [ "$EUID" != "0" ];
   then echo "Please run as root"
   exit
 fi
 echo spi > /sys/devices/platform/ocp/ocp\:P2_25_pinmux/state
 echo spi_sclk > /sys/devices/platform/ocp/ocp\:P2_29_pinmux/state
-echo spi_cs > /sys/devices/platform/ocp/ocp\:P2_31_pinmux/state
+echo spi_cs > /sys/devices/platform/ocp/ocp\:P2_25_pinmux/state
 echo -ne "\x40\x00\x00" > /dev/spidev2.1
 echo -ne "\x40\x01\x00" > /dev/spidev2.1
 echo -ne "\x40\x12\xc0" > /dev/spidev2.1
