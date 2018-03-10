670c175255a212f1de964cf9e0ce6301b4cb2d3b
Jason Kridner <jdk@ti.com>
2018-03-09 20:03:21 -0500

    spi-drivers: hack in sed command for uEnv.txt

--- RESOURCES/spi-drivers/spi-gpio.sh
+++ RESOURCES/spi-drivers/spi-gpio.sh
@@ -7,3 +7,4 @@ fi
 #mkdir -p /sys/kernel/config/device-tree/overlays/spi-gpio
 #dtc -W no-unit_address_vs_reg -@ -o /sys/kernel/config/device-tree/overlays/spi-gpio/dtbo spi-gpio.dts
 dtc -W no-unit_address_vs_reg -@ -o /lib/firmware/spi-gpio.dtbo spi-gpio.dts
+sed -i -e "s/#?uboot_overlay_addr0=.*$/uboot_overlay_addr0=\/lib\/firmware\/spi-gpio.dtbo/;" /boot/uEnv.txt
